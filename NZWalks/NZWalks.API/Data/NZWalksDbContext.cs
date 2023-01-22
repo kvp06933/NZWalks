31b1a0d3046d2c342c51e1abe4c06c763530e49f
Katherine Pitts <kvp06933@gmail.com>
2023-01-22 17:12:11 -0600

    End of Section 2

---
+++ NZWalks/NZWalks.API/Data/NZWalksDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Data
+{
+    public class NZWalksDbContext:DbContext
+    {
+        //constructor
+        public NZWalksDbContext(DbContextOptions<NZWalksDbContext> options): base(options) //passing options to base class 
+        {
+            
+        }
+        //properties
+        public DbSet<Region> Regions { get; set; }// please create a regions table if it doesn't exist in the db.
+        public DbSet<Walk> Walks { get; set; }
+        public DbSet<WalkDifficulty> WalkDifficulty { get; set; } //lookup table
+
+    }
+}
--- NZWalks/NZWalks.API/Program.cs
+++ NZWalks/NZWalks.API/Program.cs
@@ -1,4 +1,7 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Data;
+
+var builder = WebApplication.CreateBuilder(args); // creates webapplication builder class which we can use to inject the dependencies into services collection
 
 // Add services to the container.
 
@@ -7,6 +10,10 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<NZWalksDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("NZWalks"));
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
